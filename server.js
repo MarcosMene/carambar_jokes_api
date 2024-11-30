8db8ec4278472ef1d87d991d76652461676ac32f
Marcos Meneghetti <meneghettimarcos@outlook.com>
2024-11-30 17:51:53 +0100

    new address fetch for api jokes

--- server.js
+++ server.js
@@ -1,13 +1,3 @@
-require('dotenv').config();
-
-const apiUrl = process.env.NODE_ENV === 'production'
-    ? 'https://carambar-jokes-api-7d22.onrender.com/blagues/random'
-    : process.env.API_URL; // This will use 'http://localhost:4000' in development
-
-const frontendUrl = process.env.FRONTEND_URL;
-
-
-
 const express = require('express');
 const cors = require('cors');
 const apiRoutes = require('./src/routes/jokesRoutes.js');
@@ -36,25 +26,16 @@ const options = {
 const specs = swaggerJsDoc(options);
 
 const app = express();
+const port = process.env.PORT || 4000;
 
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
 
-// CORS setup
-const corsOptions = {
-  origin: [process.env.FRONTEND_URL], // Use environment variable for frontend URL
-  methods: ['GET', 'POST'],
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
+//middleware
+app.use(cors());
 app.use(express.json());
-
-//API ROUTE
 app.use('/', apiRoutes);
 
-const port = process.env.PORT || 4000;
-
 app.listen(port, async () => {
-  console.log(`Server running on ${process.env.NODE_ENV} at http://localhost:${port}`);
+  console.log(`Server is listening on port ${port}`);
   await connectToDb();
 });
